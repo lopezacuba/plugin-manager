3dfabcf9883079964e78e0897e05788f85d65d8d
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-21 19:14:32 -0700

    Create en-GB.php

---
+++ language/en-GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('PLUGIN_MANAGER_REMOVE_PLUGINS','You license is invalid. If you do not intend to maintain your Jomres subscription please uninstall all Jomres plugins.');
