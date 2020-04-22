5918046f461b11e476cf154fb9a86d9dd0f9883f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-21 19:15:14 -0700

    Add fr-FR.php

---
+++ language/fr-FR.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+ * Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 19/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('PLUGIN_MANAGER_REMOVE_PLUGINS','Votre licence est invalide ! Si vous n\'avez pas l\'intention de conserver votre abonnement à Jomres,  ou d\'en soucrire un, veuillez désinstaller tous les plug-ins.');
